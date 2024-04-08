b948810136101dcc317688df71c0d669d1094431
bieleckipiotr <72381251+bieleckipiotr@users.noreply.github.com>
2024-04-08 21:47:21 +0200

    create terraform file main.tf
    loads existing projects, both firebase and gcloud, names the project ChatRooms on gcloud and starts two basic services, which should be running with manual project creation in firebase

---
+++ main.tf
@@ -0,0 +1,82 @@
+# Terraform configuration to set up providers.
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+    }
+  }
+}
+
+
+# Configure the provider not to use the specified project for quota check.
+# This provider should only be used during project creation and initializing services.
+provider "google-beta" {
+  alias                 = "no_user_project_override"
+  user_project_override = false
+  credentials = file("D:/school/Masters/sem1/Cloud/chatroomtranslations-firebase-adminsdk-78mo9-bdfcd5f2a7.json")
+}
+
+
+# Configure the provider that uses the new project's quota.
+provider "google-beta" {
+  user_project_override = true
+  credentials = file("D:/school/Masters/sem1/Cloud/chatroomtranslations-firebase-adminsdk-78mo9-bdfcd5f2a7.json")
+}
+
+
+
+
+
+# import existing gcloud project
+import {
+  to = google_project.default
+  id = "chatroomtranslations"
+}
+
+
+resource "google_project" "default" {
+  provider = google-beta.no_user_project_override
+  project_id      = "chatroomtranslations"
+  name            = "ChatRooms"
+
+  # Required for the project to display in any list of Firebase projects.
+  labels = {
+    "firebase" = "enabled"
+  }
+}
+
+
+# Enable the required underlying Service Usage API.
+resource "google_project_service" "serviceusage" {
+  provider = google-beta.no_user_project_override
+
+  project = google_project.default.project_id
+  service = "serviceusage.googleapis.com"
+
+  # Don't disable the service if the resource block is removed by accident.
+  disable_on_destroy = false
+}
+
+
+# Enable the required underlying Firebase Management API.
+resource "google_project_service" "firebase" {
+  provider = google-beta.no_user_project_override
+
+  project = google_project.default.project_id
+  service = "firebase.googleapis.com"
+
+  # Don't disable the service if the resource block is removed by accident.
+  disable_on_destroy = false
+}
+
+
+# import existing firebase project
+import {
+  to = google_firebase_project.default
+  id = "chatroomtranslations"
+}
+
+
+resource "google_firebase_project" "default" {
+  provider = google-beta
+}
