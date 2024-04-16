b17e1ee4efd540f9d9027165967d28d1d02af843
maciejors <maciej.orslowski@gmail.com>
2024-04-16 14:43:35 +0200

    link terraform to a new gcloud project and add a firebase web app

--- main.tf
+++ main.tf
@@ -30,20 +30,32 @@ provider "google-beta" {
 # import existing gcloud project
 import {
   to = google_project.default
-  id = "chatroomtranslations"
+  id = "chatroomcloud"
 }
 
 
 resource "google_project" "default" {
   provider = google-beta.no_user_project_override
-  project_id      = "chatroomtranslations"
-  name            = "ChatRooms"
+  project_id      = "chatroomcloud"
+  name            = "ChatRoomTerraform"
 
   # Required for the project to display in any list of Firebase projects.
   labels = {
     "firebase" = "enabled"
   }
 }
+
+
+# Enables Cloud Resource Manager API.
+# resource "google_project_service" "cloudresourcemanager" {
+#   provider = google-beta.no_user_project_override
+
+#   project  = google_project.default.project_id
+#   service = "cloudresourcemanager.googleapis.com"
+
+#   # Don't disable the service if the resource block is removed by accident.
+#   disable_on_destroy = false
+# }
 
 
 # Enable the required underlying Service Usage API.
@@ -70,13 +82,24 @@ resource "google_project_service" "firebase" {
 }
 
 
-# import existing firebase project
-import {
-  to = google_firebase_project.default
-  id = "chatroomtranslations"
+resource "google_firebase_project" "default" {
+  provider = google-beta
+  project = google_project.default.project_id
 }
 
 
-resource "google_firebase_project" "default" {
-  provider = google-beta
+# Creates a Firebase Web App in the new project created above.
+resource "google_firebase_web_app" "rtdb" {
+  provider     = google-beta
+  project      = google_project.default.project_id
+  display_name = "ChatRoomsApp"
+
+  # The other App types (Android and Apple) use "DELETE" by default.
+  # Web apps don't use "DELETE" by default due to backward-compatibility.
+  deletion_policy = "DELETE"
+
+  # Wait for Firebase to be enabled in the Google Cloud project before creating this App.
+  depends_on = [
+    google_firebase_project.default,
+  ]
 }
