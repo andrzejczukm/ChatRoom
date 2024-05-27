22a607a84168cfbb3724770412026e7fa594a85c
bieleckipiotr <72381251+bieleckipiotr@users.noreply.github.com>
2024-05-28 00:06:28 +0200

    prettier-ify

--- src/routes/catalog/+page.svelte
+++ src/routes/catalog/+page.svelte
@@ -1,100 +1,99 @@
 <script>
-    import { writable} from 'svelte/store'
+	import { writable } from 'svelte/store';
 	import { getLoggedInUser } from '../../database/auth';
-    import { getRandomFolder, getImageUrlFromFolderRef, getChatName } from '../../database/catalog';
+	import { getRandomFolder, getImageUrlFromFolderRef, getChatName } from '../../database/catalog';
 	import { onMount } from 'svelte';
-    import { db } from '../../database/config';
-    import {
-	collection,
-	query,
-	where,
-	getDocs,
-} from 'firebase/firestore';
+	import { db } from '../../database/config';
+	import { collection, query, where, getDocs } from 'firebase/firestore';
 
-const chatRoomsCollection = collection(db, 'chatRooms');
-    let user = null;
-    let imageUrls = writable([]); // Array to store image URLs
+	const chatRoomsCollection = collection(db, 'chatRooms');
+	let user = null;
+	let imageUrls = writable([]); // Array to store image URLs
 
+	onMount(async () => {
+		user = await getLoggedInUser();
+		if (user) {
+			const userChatsQuery = query(
+				chatRoomsCollection,
+				where('members', 'array-contains', user.id)
+			);
+			const userChats = await getDocs(userChatsQuery);
+			const urls = [];
 
-    onMount(async () => {
-        user = await getLoggedInUser();
-        if (user) {
-            const userChatsQuery = query(chatRoomsCollection, where('members', 'array-contains', user.id));
-            const userChats = await getDocs(userChatsQuery);
-            const urls = [];
+			for (const chat of userChats.docs) {
+				const folderRef = await getRandomFolder(chat.id);
+				const chatName = await getChatName(chat.id);
+				if (folderRef) {
+					const url = await getImageUrlFromFolderRef(folderRef);
+					if (url) {
+						urls.push({ imageUrl: url, chatName: chatName, chatId: chat.id }); // Add URL to the temporary array
+					}
+				}
+			}
 
-            for (const chat of userChats.docs) {
-                const folderRef = await getRandomFolder(chat.id);
-                const chatName = await getChatName(chat.id)
-                if (folderRef) {
-                    const url = await getImageUrlFromFolderRef(folderRef);
-                    if (url) {
-                        urls.push({ imageUrl: url, chatName: chatName, chatId : chat.id }); // Add URL to the temporary array
-                    }
-                }
-            }
-
-            // Update the writable store with the array of image URLs
-            imageUrls.set(urls);
-        }
-    });
-
-    
+			// Update the writable store with the array of image URLs
+			imageUrls.set(urls);
+		}
+	});
 </script>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <div class="grid-container">
-    {#each $imageUrls as imageObj}
-    <div>
-        <figure>
-            <a href="catalog/{imageObj.chatId}">
-            <div class="image-wrapper">
-            <img src="{imageObj.imageUrl}" alt="{imageObj.chatId}"/></div></a>
-            <a href="catalog/{imageObj.chatId}">
-            <!-- svelte-ignore a11y-structure -->
-            <figcaption>{imageObj.chatName}</figcaption></a>
-        </figure>
-    </div>
-{/each}
+	{#each $imageUrls as imageObj}
+		<div>
+			<figure>
+				<a href="catalog/{imageObj.chatId}">
+					<div class="image-wrapper">
+						<img src={imageObj.imageUrl} alt={imageObj.chatId} />
+					</div></a
+				>
+				<a href="catalog/{imageObj.chatId}">
+					<!-- svelte-ignore a11y-structure -->
+					<figcaption>{imageObj.chatName}</figcaption></a
+				>
+			</figure>
+		</div>
+	{/each}
 </div>
 
 <style>
-    img {
-        width: 100%;
-        height: 100%;
-        display: block; 
-    }
-    
-    .grid-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 10px;
-    /* background-color: #709692; */
-    padding: 10px;
-    justify-content: space-evenly;
-    align-content: center;
-    }
+	img {
+		width: 100%;
+		height: 100%;
+		display: block;
+	}
 
-    .image-wrapper {
-    width: 256px; 
-    height: 256px; 
-    margin: 10px; 
-    overflow: hidden; 
-    background-color: white;
-    justify-content: center;
-    align-items: center;
-}
+	.grid-container {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 10px;
+		/* background-color: #709692; */
+		padding: 10px;
+		justify-content: space-evenly;
+		align-content: center;
+	}
 
-    * {
-    box-sizing: border-box;
-    }
+	.image-wrapper {
+		width: 256px;
+		height: 256px;
+		margin: 10px;
+		overflow: hidden;
+		background-color: white;
+		justify-content: center;
+		align-items: center;
+	}
 
-    figcaption {
-    text-align: center;
-    color : #709692;
-    border : none;
-    text-decoration:none;
-    }
-    a {
-        text-decoration:none;
-    }
+	* {
+		box-sizing: border-box;
+	}
+
+	figcaption {
+		text-align: center;
+		color: #709692;
+		border: none;
+		text-decoration: none;
+	}
+	a {
+		text-decoration: none;
+	}
 </style>
